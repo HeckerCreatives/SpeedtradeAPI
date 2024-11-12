fcfafa9468d5fa120fa09dca9a11cb16e77506df
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-11-12 10:08:00 +0800

    poosh

--- controllers/payin.js
+++ controllers/payin.js
@@ -138,7 +138,7 @@ exports.deletepayinplayersuperadmin = async (req, res) => {
         return res.status(400).json({message: "bad-request", data: "There's a problem changing the transaction Please contact customer support!"})
     })
 
-    await Userwallets.findOneAndUpdate({owner: new mongoose.Types.ObjectId(userid), type: "fiatbalance"}, {$inc: {amount: -transaction.value}})
+    await Userwallets.findOneAndUpdate({owner: new mongoose.Types.ObjectId(userid), type: "minecoinwallet"}, {$inc: {amount: -transaction.value}})
     .catch(async err => {
         console.log(`There's a problem getting the changing the transaction ${transactionid}. Error: ${err}`)
 
@@ -147,7 +147,7 @@ exports.deletepayinplayersuperadmin = async (req, res) => {
         return res.status(400).json({message: "bad-request", data: "There's a problem changing the transaction Please contact customer support!"})
     })
 
-    await deleteanalytics(transactionid)
+    await deleteanalytics(transaction._id)
 
     return res.json({message: "success"})
 }

0530c567f21743d430bf183693f2f0fa1ab65760
HeckerCreatives <daniel.janbiengabrielle@gmail.com>
2024-11-13 11:29:47 +0800

    Update payin.js

--- controllers/payin.js
+++ controllers/payin.js
@@ -141,6 +141,8 @@ exports.deletepayinplayersuperadmin = async (req, res) => {
         return res.status(400).json({message: "bad-request", data: "There's a problem changing the transaction Please contact customer support!"})
     })
 
+    console.log(userid, transaction.value)
+
     await Userwallets.findOneAndUpdate({owner: new mongoose.Types.ObjectId(userid), type: "creditwallet"}, {$inc: {amount: -transaction.value}})
     .catch(async err => {
         console.log(`There's a problem getting the changing the transaction ${transactionid}. Error: ${err}`)

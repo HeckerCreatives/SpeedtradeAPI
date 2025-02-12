fb06443ab8ed34ab62a9d843c08d3a8f36c1a26e
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-02-12 16:57:45 +0800

    pooosh

--- initialization/serverinitialize.js
+++ initialization/serverinitialize.js
@@ -6,6 +6,7 @@ const Userdetails = require("../models/Userdetails")
 const Maintenance = require("../models/Maintenance")
 const Miner = require("../models/Miner")
 const Pricepool = require("../models/Pricepool")
+const Skip = require("../models/Skip")
 
 exports.initialize = async (req, res) => {
 
@@ -153,6 +154,29 @@ exports.initialize = async (req, res) => {
         })
     }
 
+    // find users created at before february 1 2025
+
+    const users = await Users.find({createdAt: {$lt: new Date("2025-02-01")}})
+// check if they have skip if not create for them
+    if(users.length > 0){
+        users.forEach(async (user) => {
+            await Skip.findOne({owner: new mongoose.Types.ObjectId(user._id)})
+
+            .then(async data => {
+                if(!data){
+                    await Skip.create({owner: new mongoose.Types.ObjectId(user._id)})
+                }
+            }
+
+            )
+            .catch(err => {
+                console.log(`There's a problem getting skip data ${err}`)
+                return
+            })
+        }
+        )
+    }
+            
 
     console.log("Server Initialization Success")
 }

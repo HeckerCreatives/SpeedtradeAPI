6d1048c3ce507ae92f0e745782ff62b7bd058d94
HeckerCreatives <daniel.janbiengabrielle@gmail.com>
2025-02-18 23:39:41 +0800

    pooosh

--- controllers/miner.js
+++ controllers/miner.js
@@ -57,36 +57,47 @@ exports.getUserMiner = async(req, res)=> {
     } 
     else if (type == "flash_miner"){
 
-        // const tempminer = await Inventoryhistory.findOne({owner: new mongoose.Types.ObjectId(id), minertype: "swift_lane", type: "Buy Switf Lane"})
-        // .then(data => data)
-        // const tempminer1 = await Inventoryhistory.findOne({owner: new mongoose.Types.ObjectId(id), minertype: "quick_miner", type: "Buy Quick Miner"})
-        // .then(data => data)
-        // const tempminer2 = await Inventoryhistory.findOne({owner: new mongoose.Types.ObjectId(id), minertype: "rapid_lane", type: "Buy Rapid Lane"})
-        // .then(data => data)
+        const tempminer = await Inventoryhistory.findOne({owner: new mongoose.Types.ObjectId(id), minertype: "swift_lane", type: "Buy Switf Lane"})
+        .then(data => data)
+        const tempminer1 = await Inventoryhistory.findOne({owner: new mongoose.Types.ObjectId(id), minertype: "quick_miner", type: "Buy Quick Miner"})
+        .then(data => data)
+        const tempminer2 = await Inventoryhistory.findOne({owner: new mongoose.Types.ObjectId(id), minertype: "rapid_lane", type: "Buy Rapid Lane"})
+        .then(data => data)
 
-        // if(!tempminer || !tempminer1 || !tempminer2){
-        //     value = false
-        // }
+        if(!tempminer || !tempminer1 || !tempminer2){
+            value = false
+        }
         
         //  REMOVED SKIP REQUESTED BY CLIENT, JUST UNCOMMENT IF THE CLIENT WANTS TO HAVE SKIP AGAIN AND COMMENT THE VALUE = TRUE
 
-        value = true
+        // value = true
         
     }
 
-    //  REMOVE THE IF STATEMENT AND CUT THE CODE INSIDE THE IF STATEMENT IF THE CLIENT WANTS THE FLASH MINER TO HAVE SKIP AGAIN
-    if (type == "swift_lane" || type == "rapid_lane"){
-        const isskip = await Skip.findOne({owner: new mongoose.Types.ObjectId(id)})
-        .then(data => data)
-        .catch(err => {
-            console.log(`There's a problem fetching skip. Error: ${err}`)
-            return res.status(400).json({ message: "bad-request", data: "There's a problem with the server. Please contact customer support for more details."})
-        })
-    
-        if(isskip !== null){
-            value = true
-        }
+    const isskip = await Skip.findOne({owner: new mongoose.Types.ObjectId(id)})
+    .then(data => data)
+    .catch(err => {
+        console.log(`There's a problem fetching skip. Error: ${err}`)
+        return res.status(400).json({ message: "bad-request", data: "There's a problem with the server. Please contact customer support for more details."})
+    })
+
+    if(isskip !== null){
+        value = true
     }
+
+    //  REMOVE THE IF STATEMENT AND CUT THE CODE INSIDE THE IF STATEMENT IF THE CLIENT WANTS THE FLASH MINER TO HAVE SKIP AGAIN
+    // if (type == "swift_lane" || type == "rapid_lane"){
+    //     const isskip = await Skip.findOne({owner: new mongoose.Types.ObjectId(id)})
+    //     .then(data => data)
+    //     .catch(err => {
+    //         console.log(`There's a problem fetching skip. Error: ${err}`)
+    //         return res.status(400).json({ message: "bad-request", data: "There's a problem with the server. Please contact customer support for more details."})
+    //     })
+    
+    //     if(isskip !== null){
+    //         value = true
+    //     }
+    // }
 
     return res.status(200).json({ message: "success", data: value})
 }
